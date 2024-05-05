55013fe6653e9aee3573ba5594ad050011e6f955
KhushaliDudhabade <131711092+KhushaliDudhabade@users.noreply.github.com>
2024-05-05 18:42:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function getFormvalue() {
-    //Write your code here
+    
+    var form = document.getElementById("myForm");
 
+// Get the values of the first and last name fields
+var firstName = form.elements["fname"].value;
+var lastName = form.elements["lname"].value;
+var submitBtn=document.getElementById("submit");
+
+	// function myFunction(){
+	// 	// alert(first_name+" "+last_name);
+	// 	alert("successfully subimited");
+	// }
+    
+     submitBtn.addEventListener("click", () => {
+        alert(first_name+" "+last_name);
+  });
 }
